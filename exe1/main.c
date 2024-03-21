696e8d44177a939b5d9591d39d070491db8e1941
Mariana <marianaaa1@al.insper.edu.br>
2024-03-21 23:42:20 +0000

    testando 4

--- exe1/main.c
+++ exe1/main.c
@@ -12,23 +12,22 @@
 void adc_task(void *p) {
     adc_init();
     adc_gpio_init(27);
+    adc_gpio_init(26);
 
-    // Fator de conversão para 12 bits, supondo valor máximo == ADC_VREF == 3.3 V
+    // Fator de conversão para 12 bits, assumindo valor máximo == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
 
-    uint16_t result;
+    uint16_t result_1, result_2;
     while (1) {
-        // Iteração 1
-        adc_select_input(1); // Seleciona entrada ADC 1 (GPIO27)
-        result = adc_read();
-        printf("Voltagem 1: %f V\n", result * conversion_factor);
+        // Leitura do potenciômetro no ADC 1 (GP27)
+        adc_select_input(1);
+        result_1 = adc_read();
+        printf("voltage 1: %f V\n", result_1 * conversion_factor);
 
-        // CÓDIGO IMPORTANTE AQUI
-
-        // Iteração 2
-        adc_select_input(2); // Seleciona entrada ADC 2 (GPIO28)
-        result = adc_read();
-        printf("Voltagem 2: %f V\n", result * conversion_factor);
+        // Leitura do potenciômetro no ADC 0 (GP26)
+        adc_select_input(0);
+        result_2 = adc_read();
+        printf("voltage 2: %f V\n", result_2 * conversion_factor);
 
         vTaskDelay(pdMS_TO_TICKS(200));
     }
@@ -39,7 +38,7 @@ int main() {
     printf("Iniciando RTOS\n");
     adc_init();
 
-    xTaskCreate(adc_task, "ADC_Task", 4095, NULL, 1, NULL);
+    xTaskCreate(adc_task, "ADC_Task", 4096, NULL, 1, NULL);
     vTaskStartScheduler();
     
     while (true) {

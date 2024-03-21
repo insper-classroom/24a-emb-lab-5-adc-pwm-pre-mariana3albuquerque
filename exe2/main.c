83dee50045afdd495e8f70cce3eff6a4ccba2e27
Mariana <marianaaa1@al.insper.edu.br>
2024-03-21 23:54:16 +0000

    ex2

--- exe2/main.c
+++ exe2/main.c
@@ -14,15 +14,10 @@ void led_task(void *p) {
     uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
     pwm_set_clkdiv(slice_num, 125);
     pwm_set_wrap(slice_num, 100);
-    
-    // Configurar o Duty Cycle para 20%
-    int duty_cycle = 20; // 20% de Duty Cycle
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, duty_cycle * pwm_get_wrap(slice_num) / 100);
-    
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 80);
     pwm_set_enabled(slice_num, true);
 
     while (true) {
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Adicione um pequeno atraso para manter a tarefa ativa
     }
 }
 

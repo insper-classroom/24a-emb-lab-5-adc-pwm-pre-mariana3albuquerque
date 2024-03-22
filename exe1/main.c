9ae6caa560be2b30b572b74b00f1bcfb7e55ced5
Mariana <marianaaa1@al.insper.edu.br>
2024-03-22 00:07:29 +0000

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -11,20 +11,22 @@
 
 void adc_task(void *p) {
     adc_init();
-    adc_gpio_init(27);
-    adc_gpio_init(26);
+
+    // Configuração dos pinos ADC
+    adc_gpio_init(27); // GP27 para leitura do potenciômetro 1
+    adc_gpio_init(26); // GP26 para leitura do potenciômetro 2
 
     // Fator de conversão para 12 bits, assumindo valor máximo == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
 
     uint16_t result_1, result_2;
     while (1) {
-        // Leitura do potenciômetro no ADC 1 (GP27)
+        // Leitura do potenciômetro 1 no ADC 1 (GP27)
         adc_select_input(1);
         result_1 = adc_read();
         printf("voltage 1: %f V\n", result_1 * conversion_factor);
 
-        // Leitura do potenciômetro no ADC 0 (GP26)
+        // Leitura do potenciômetro 2 no ADC 0 (GP26)
         adc_select_input(0);
         result_2 = adc_read();
         printf("voltage 2: %f V\n", result_2 * conversion_factor);
